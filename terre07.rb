fce81a2f974015b88805b63393e05850cd2616e9
Alathea FERRAND <alatheaferrand@gmail.com>
2025-02-13 18:59:46 -1000

    Taille d'une chaîne

---
+++ terre07.rb
@@ -0,0 +1,20 @@
+# Taille d'une chaîne
+# Affiche le nombre de caractères d'une chaîne passée en argument
+# Sans utiliser length ni size
+
+input = ARGV[0]
+
+if ARGV[1]
+  puts 'erreur'
+  exit
+elsif ARGV.empty?
+  puts 'erreur'
+  exit
+elsif input.match?(/\A-?\d+(\.\d+)?\z/)
+  puts 'erreur'
+  exit
+end
+
+count = 0
+input.each_char { count += 1 }
+puts count
