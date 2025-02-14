24228660008aacb6729eef5bf64c28f088b73a5b
Alathea FERRAND <alatheaferrand@gmail.com>
2025-02-13 14:13:04 -1000

    L'Alphabet

---
+++ terre00.rb
@@ -0,0 +1,14 @@
+# L'alphabet
+# Affiche l'alphabet en lettres minuscules suivi d'un retour à la ligne
+
+alphabet = ""
+
+for n in 'a'..'z'
+  alphabet << n
+end
+
+puts "#{alphabet}\n"
+
+# ('a'..'z').each do |letter|
+#   alphabet << letter
+# end
