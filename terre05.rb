4eaa521305a3d2dfbe7fc064e7deab66f5316ded
Alathea FERRAND <alatheaferrand@gmail.com>
2025-02-13 19:08:01 -1000

    Divisions

--- terre05.rb
+++ terre05.rb
@@ -1,7 +1,7 @@
 # Divisions
 # Affiche le résultat et le reste d'une division entre 2 nombres
 
-if ARGV[0].to_i && ARGV[1].to_i > 0 && ARGV[0].to_i >= ARGV[1].to_i
+if ARGV[1].to_i > 0 && ARGV[0].to_i >= ARGV[1].to_i
   x = ARGV[0].to_i
   y = ARGV[1].to_i
   puts "résultat : #{ (x/y) }"
