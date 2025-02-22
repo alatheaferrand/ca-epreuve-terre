f74fab200aa34706cebc2aea4a9f1233bbd1c81b
Alathea FERRAND <alatheaferrand@gmail.com>
2025-02-21 16:46:30 -1000

    Nom du progamme : sans utiliser de méthode native

--- terre01.rb
+++ terre01.rb
@@ -1,6 +1,25 @@
-# frozen_string_literal: true
-
 # Nom du programme
 # Programme qui affiche son nom de fichier
 
-puts File.basename(__FILE__)
+# Récupération du chemin complet du fichier
+path = __FILE__
+
+# Trouver l'index du dernier séparateur (sans méthode native)
+last_slash_index = -1
+index = 0
+
+while path[index]
+  last_slash_index = index if path[index] == '/' || path[index] == '\\' # Windows & Linux/Mac
+index += 1
+end
+
+# Extraction du nom du fichier
+filename = ''
+index = last_slash_index + 1
+
+while path[index]
+  filename << path[index]
+  index += 1
+end
+
+puts filename
