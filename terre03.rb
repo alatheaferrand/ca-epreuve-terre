63c51bb174e01a35b9ae79584d5189f99a4f4281
Alathea FERRAND <alatheaferrand@gmail.com>
2025-02-22 13:49:06 -1000

    L'alphabet à partir de : optimisation de la gestion ASCII

--- terre03.rb
+++ terre03.rb
@@ -2,13 +2,18 @@
 # Affiche l’alphabet à partir de la lettre donnée en argument en lettres minuscules
 # suivi d’un retour à la ligne
 
-letter = ARGV[0].downcase
-alphabet = ''
+# Récupération et conversion de l'argument en décimal (code ASCII)
+first_letter = ARGV[0].ord
 
-for n in (letter..'z')
-  alphabet << n
+# Conversion en minuscule
+first_letter = (first_letter + 32) if first_letter <= 90 && first_letter >= 65
+
+alphabet = ''
+i = first_letter
+
+while i <= 122 # Code ASCII de 'z'
+  alphabet << i.chr
+  i += 1
 end
 
-# alphabet = (letter..'z').to_a.join
-
-puts "#{alphabet}\n"
+puts alphabet
