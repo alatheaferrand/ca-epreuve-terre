a089d95dc72dc02acbc1110d8024dbdfd7657aa8
Alathea FERRAND <alatheaferrand@gmail.com>
2025-02-22 14:40:46 -1000

    L'alphabet à partir de : correction FrozenError et respect des string literals

--- terre03.rb
+++ terre03.rb
@@ -1,19 +1,14 @@
 # frozen_string_literal: true
 
 # L'alphabet à partir de
-# Affiche l’alphabet à partir de la lettre donnée en argument en lettres minuscules
+# Affiche l’alphabet à partir de la lettre donnée en argument en lettres minuscules,
 # suivi d’un retour à la ligne.
 
 first_letter = ARGV[0].ord
+first_letter += 32 if first_letter.between?(65, 90) # Convertit en minuscule si nécessaire
 
-# Convertit en minuscule si l'argument est une majuscule
-first_letter += 32 if first_letter.between?(65, 90)
+alphabet = String.new #  Crée une chaîne mutable
 
-alphabet = String.new
-
-while first_letter <= 122 # ASCII de 'z'
-  alphabet << first_letter.chr
-  first_letter += 1
-end
+(first_letter..122).each { |i| alphabet << i.chr }
 
 puts alphabet
