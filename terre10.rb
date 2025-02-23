0e7c46d659b8283ec68a64410b7ea4ae9d122260
Alathea FERRAND <alatheaferrand@gmail.com>
2025-02-22 16:49:56 -1000

    Nombre premier : correction RuboCop et amélioration de la gestion des cas particuliers

--- terre10.rb
+++ terre10.rb
@@ -1,62 +1,43 @@
+# frozen_string_literal: true
+
 # Nombre premier
-# Affiche si un nombre est premier ou pas
-# Attention : 0 et 1 ne sont pas des nombres premiers
+# Affiche si un nombre est premier ou non.
+# Attention : 0 et 1 ne sont pas des nombres premiers.
+# Gérer les erreurs d'arguments.
 
-entier = ARGV[0]
-en_trop = ARGV[1]
-
-# Vérifier qu'il n'y a qu'un seul argument
-if en_trop || entier.nil?
+# Vérification des erreurs
+if ARGV.size != 1 || ARGV[0].to_i.to_s != ARGV[0] || ARGV[0].to_i.negative?
   puts 'erreur'
   exit
 end
 
-# Vérifier que l'argument est bien un nombre entier
-if !entier.match?(/\A\d+\z/)
-  puts 'erreur'
+entier = ARGV[0].to_i
+
+# Cas particuliers
+if [0, 1].include?(entier)
+  puts "Non, #{entier} n'est pas un nombre premier."
+  exit
+elsif entier == 2
+  puts 'Oui, 2 est un nombre premier.'
+  exit
+elsif (entier % 2).zero?
+  puts "Non, #{entier} n'est pas un nombre premier."
   exit
 end
 
-# Conversion en integer
-entier = entier.to_i
+# Vérification de la primalité en testant uniquement jusqu'à la racine carrée
+racine = 1
+racine += 1 while racine * racine <= entier
 
-# Première vérification
-if entier <= 1
-    puts "Non, #{entier} n'est pas un nombre premier."
-    exit
-  elsif entier == 2
-    puts "Oui, 2 est un nombre premier."
-    exit
-  elsif entier % 2 == 0 # exclus tous les nombres pairs autre que 2
-    puts "Non, #{entier} n'est pas un nombre premier."
-    exit
-  end
+is_prime = true
+i = 3
 
-# Calcul de la racine carré
-racine = 0
-until racine * racine >= entier
-  racine += 1
-end
-
-# S'assurer que racine est impair pour la prochaine boucle
-if racine % 2 == 0
-  racine -= 1
-end
-
-# Vérifier s'il existe un diviseur impair entre 3 et la racine
-diviseur = false
-while racine >= 3
-  if entier % racine == 0
-    diviseur = true
+while i <= racine
+  if (entier % i).zero?
+    is_prime = false
     break
   end
-  racine -= 2
+  i += 2 # On saute les nombres pairs
 end
 
-# Résultat final
-if diviseur
-  puts "Non, #{entier} n'est pas un nombre premier."
-else
-  puts "Oui, #{entier} est un nombre premier."
-end
- 
+puts is_prime ? "Oui, #{entier} est un nombre premier." : "Non, #{entier} n'est pas un nombre premier."
