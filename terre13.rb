2428ad077c7c025b419b464b608eb4f6fa6d2404
Alathea FERRAND <alatheaferrand@gmail.com>
2025-02-25 08:42:48 -1000

    Trouver la Suisse : optimisation Ruby-friendly

--- terre13.rb
+++ terre13.rb
@@ -1,44 +1,29 @@
+# frozen_string_literal: true
+
 # Trouver la Suisse
 # Prend en paramètres 3 entiers et affiche la valeur du milieu
-# Sans utiliser la fonction sort
+# Fonction interdite: la fonction sort
 
-# Vérifier les arguments : 3 nombres entiers positifs
-if ARGV[0].nil? || ARGV[1].nil? || ARGV[2].nil? || ARGV[3]
-  puts 'erreur'
-  exit
-elsif !ARGV[0].match?(/\A\d+\z/) || !ARGV[1].match?(/\A\d+\z/) || !ARGV[2].match?(/\A\d+\z/) 
+# Vérification des erreurs
+if ARGV.size != 3 || ARGV.any? { |arg| !arg.match?(/\A-?\d+\z/) }
   puts 'erreur'
   exit
 end
+
+# Conversion en entiers
+a, b, c = ARGV.map(&:to_i)
 
 # Vérifier que les arguments sont différents
-if ARGV[0] == ARGV[1] || ARGV[1] == ARGV[2] || ARGV[0] == ARGV[2]
+if a == b || b == c || a == c
   puts 'erreur'
   exit
 end
 
-# Récupérer les arguments
-a = ARGV[0].to_i
-b = ARGV[1].to_i
-c = ARGV[2].to_i
-val_max = a
-val_min = a
-
-# Comparer : trouver le maximum et le minimum pour déduire la valeur médiane
-if b > val_max
-  val_max = b
-elsif c > val_max
-  val_max = c
+# Trouver la valeur du milieu
+if (a > b && a < c) || (a > c && a < b)
+  puts a
+elsif (b > a && b < c) || (b > c && b < a)
+  puts b
+else
+  puts c
 end
-
-if b < val_min 
-    val_min = b
-elsif c < val_min
-    val_min = c
-end
-
-# Déduire la valeur médiane
-val_milieu = ( a + b + c ) - ( val_max + val_min )
-
-# Affichage du résultat
-puts val_milieu
