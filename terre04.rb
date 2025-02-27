27a7a19655c05a4b0e2d454d38785e73ab753b5c
Alathea FERRAND <alatheaferrand@gmail.com>
2025-02-26 16:59:04 -1000

    [Pair ou impair] Fix : improve numeric validation

--- terre04.rb
+++ terre04.rb
@@ -4,6 +4,10 @@
 # Détermine si l'argument donné est pair ou impair, y compris pour les nombres négatifs.
 # Fonctions interdites : even? et odd?
 
+def numeric?(number)
+  Integer(number, exception: false) ? true : false
+end
+
 args = ARGV
 
 if args.size != 1
@@ -13,7 +17,7 @@ end
 
 number = args[0]
 
-if number =~ /^-?\d+$/ # Vérifie que l'entrée est bien un entier (positif ou négatif)
+if numeric?(number)
   number = number.to_i
   puts (number % 2).zero? ? 'pair' : 'impair'
   exit
