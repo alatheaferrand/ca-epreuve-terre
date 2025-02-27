b8e3bbdab15f74d9c2b2a1149b8a8cd0e0223a52
Alathea FERRAND <alatheaferrand@gmail.com>
2025-02-27 11:10:51 -1000

    [Puissance d'un nombre] Refactor: improve error messages translation

--- terre08.rb
+++ terre08.rb
@@ -5,21 +5,39 @@
 # L'exposant ne pourra pas être négatif.
 # Fonction interdite: la fonction pow.
 
+def numeric?(str)
+  !Integer(str, exception: false).nil?
+end
+
+args = ARGV
+
 # Vérification des erreurs
-if ARGV.size != 2 || ARGV[0].to_i.to_s != ARGV[0] || ARGV[1].to_i.to_s != ARGV[1] || ARGV[1].to_i.negative?
-  puts 'erreur'
+if args.size != 2
+  puts 'error: 2 arguments expected'
   exit
 end
 
-# Conversion en entiers
-nombre = ARGV[0].to_i
-exposant = ARGV[1].to_i
-
-# Calcul de la puissance (sans pow)
-resultat = 1
-while exposant.positive?
-  resultat *= nombre
-  exposant -= 1
+number, exponent = args.each do |number|
+  unless numeric?(number)
+    puts 'error: arguments must be numbers'
+    exit
+  end
 end
 
-puts resultat
+number = number.to_i
+exponent = exponent.to_i
+
+if exponent.negative?
+  puts 'error: the exponent must be positive'
+  exit
+end
+
+# Calcul de la puissance (sans pow)
+result = 1
+i = 0
+while i < exponent
+  result *= number
+  i += 1
+end
+
+puts result
