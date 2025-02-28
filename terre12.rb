c40d46a3653a3142f4b3b04ca38fb186ab0e074b
Alathea FERRAND <alatheaferrand@gmail.com>
2025-02-28 09:32:16 -1000

    [12 to 24] refactor: improve variable handling

--- terre12.rb
+++ terre12.rb
@@ -4,23 +4,29 @@
 # Convertit une une 12h AM/PM en format 24h
 # Attention: midi et minuit
 
-# Vérification des erreurs
-if ARGV.size != 1 || !ARGV[0].match?(/\A(0[1-9]|1[0-2]):[0-5]\d[AP]M\z/)
-  puts 'erreur'
+args = ARGV
+time_format_regex = /\A(0[1-9]|1[0-2]):([0-5]\d) ([AP]M|[ap]m)\z/
+
+# Error handling
+if args.size != 1
+  puts 'error: 1 argument expected'
   exit
 end
 
-# Extraction des heures, minutes et suffixe
-heure = ARGV[0]
-heures = heure[0..1]
-minutes = heure[3..4]
-suffixe = heure[5]
-
-# Conversion 12h → 24h
-if suffixe == 'A'
-  heures = '00' if heures == '12'
-elsif suffixe == 'P' && heures != '12'
-  heures = heures.to_i + 12
+unless args[0].match? time_format_regex
+  puts 'error: argument must be in HH:MM AM/PM format, e.g., 11:45 PM'
+  exit
 end
 
-puts "#{heures}:#{minutes}"
+# Parsing
+hours, minutes, meridian = args[0].split(/[: ]/)
+meridian = meridian.downcase
+hours = hours.to_i
+minutes = minutes.to_i
+
+# Conversion 12h → 24h
+hours = 0 if hours == 12 && meridian == 'am'
+
+hours += 12 if hours != 12 && meridian == 'pm'
+
+puts "#{format('%02d', hours)}:#{format('%02d', minutes)}"
